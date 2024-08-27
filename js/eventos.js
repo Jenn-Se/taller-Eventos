a76e4ab811d72c7b52566cde99898cc83d0f9fd3
jennSee <sivis3.js@gmail.com>
2024-08-27 12:13:23 -0300

    solucion a la propagacion de eventos

--- js/eventos.js
+++ js/eventos.js
@@ -1,7 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
- 
+
     document.getElementById('contenedor').addEventListener('click', function() {
         alert('Hola! Soy el div');
     });
-});
 
+    document.getElementById('boton').addEventListener('click', function(event) {
+        event.stopPropagation(); 
+        alert('Hola! Soy el botón');
+    });
+});
